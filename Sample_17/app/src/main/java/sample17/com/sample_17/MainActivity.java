498731bf2ff5cbc5a168c1fd0ffbbf749ecd15cd
kenXtaro <ken.x@outlook.jp>
2016-02-01 23:50:24 +0900

    Add camera preview

--- Sample_17/app/src/main/java/sample17/com/sample_17/MainActivity.java
+++ Sample_17/app/src/main/java/sample17/com/sample_17/MainActivity.java
@@ -1,52 +1,43 @@
 package sample17.com.sample_17;
 
+import android.hardware.Camera;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Camera mCam = null;
+    private CameraPreview mCamPreview = null;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // カメラインスタンスの取得
+        try {
+            mCam = Camera.open();
+            mCam.setDisplayOrientation(90);
+        } catch (Exception e) {
+            // エラー
+            this.finish();
         }
 
-        return super.onOptionsItemSelected(item);
+        // FrameLayout に CameraPreview クラスを設定
+        FrameLayout preview = (FrameLayout)findViewById(R.id.cameraPreview);
+        mCamPreview = new CameraPreview(this, mCam);
+        preview.addView(mCamPreview);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // カメラ破棄インスタンスを解放
+        if (mCam != null) {
+            mCam.release();
+            mCam = null;
+        }
     }
 }
